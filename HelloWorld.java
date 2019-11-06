81f462717de581890a1e3b94a1b71b283324a411
maodaping <maomao@mails.ccnu.edu.cn>
2019-11-06 22:40:48 +0800

    branch1 commit2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 	public static void main (String[] args){
-		System.out.println("branch1");
+		System.out.println("branch111111");
 		System.out.println("first file.");
 	
 	}
